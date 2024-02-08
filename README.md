4f9328ce3fc3afd03f44b1c5e09321097cec95a1
Isaac J <duckhunterjr@gmail.com>
2024-02-08 15:23:44 -0800

    add completed assignment

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# Run a Basic Express NodeJS Server
+# Integrating Third-Party APIs: Spoonacular
+
+## Introduction
+The following application is in it's first iteration. It uses [Spoonacular API](https://spoonacular.com/food-api) to fetch recipes for cooks who want an ad free experience. No more scrolling through blog content, Yay! After a user queries in the text field, a list of links are generated based on Spoonacular API response. When a user clicks on a recipe, the ID of the recipe is passed to a GET method route which handles an API call fetching details to the specific recipe. A table is generated which include ingredient, amount, unit, and aisle. 
+
+Technologies: Express.js, Axios and EJS
+
+## Demo
+![Live demo](./assets/demo.gif)
+
+# Run The Application
 
 ## How to Run the Server
 1. [Clone](https://docs.github.com/en/repositories/creating-and-managing-repositories/cloning-a-repository) or [Fork](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/working-with-forks/fork-a-repo) the repository from Github.
@@ -11,5 +21,4 @@
 - The server uses [Express](https://www.npmjs.com/package/express) to handle site routing.
 - Any errors relating to node version incompatibility, ensure to switch to **node version 20.11.0** using [nvm](https://www.freecodecamp.org/news/node-version-manager-nvm-install-guide/) or [fnm](https://www.freecodecamp.org/news/fnm-fast-node-manager/)
 - Use `npx nodemon index.js` to initiate [nodemon](https://www.npmjs.com/package/nodemon) for server development. Use **ctrl + c** to terminate nodemon execution. 
-- Install eslint by running `npm init @eslint/config` in terminal. Use `npx eslint index.js` to run linter. Feel free to [configure lint rules](https://eslint.org/docs/latest/use/configure/rules) in .eslintrc.json
-![image of eslint in terminal](./img/eslint.png)
+
--- index.js
+++ index.js
@@ -1,38 +1,101 @@
 import express from 'express';
-//import { APICredentials } from './apiCred';
-//const axios = require('axios');
+import { APICredentials } from './apiCred.js';
+import axios from 'axios';
+
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
+app.set("view engine", 'ejs');
 app.set('port', port);
 
-app.get("/", (req, res) => {
-    res.type('text/plain');
-    res.send("recipe page");
-});
 
-// Query grocery API
-app.get('/recipe', (req, res) => {
-    const userQuery = req.query;
-
-    let jsonResponse ;
-
-    // Verify query property
-    if (userQuery.hasOwnProperty('query')) {
-        console.log(userQuery);
-
-
-        
+async function getSpoonacularAPIResponse(inputObj = {}, id = null) {
+    console.log(inputObj, id);
+    const options = {
+        method: 'GET',
+        url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch',
+        headers: APICredentials
+    };
+    if (id === null && inputObj.hasOwnProperty('query')) {
+        options.params = { query: inputObj.query };
     } else {
-        jsonResponse = { 'success': false }
-        res.status(406).type('json').send(jsonResponse)
+        options.url = `https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`;
     }
 
-    res.type('text/plain');
-    res.send("Query recipe page");
-    console.log(userQuery);
+    try {
+        const response = await axios.request(options);
+        console.log("Fetch status: " + response.status);
+       //console.log(response.data);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.get("/", (req, res) => {
+    res.type('html');
+    res.render("recipes", { data: {} });
+});
+/*
+TODO:
+    [x] Use id to fetch recipe 
+    [x] Get array, render to ejs
+*/
+app.get('/recipe/:id', async (req, res) => {
+    const urlId = req.params.id;
+    const parseNum = Math.abs(parseInt(urlId));
+
+    let jsonResponse;
+
+    if (!isNaN(parseNum)) {
+        jsonResponse = await getSpoonacularAPIResponse(null, parseNum);
+
+        // Get ingredients 
+        const ingredientArr = jsonResponse.extendedIngredients
+        console.log(jsonResponse)
+       
+        res.type('text/html');
+        res.render("ingredients", { ingredientArr: ingredientArr, recipeTitle: jsonResponse.title, instructions:jsonResponse.instructions });
+
+    } else {
+        jsonResponse = { 'success': false };
+        res.status(406).type('json').send(jsonResponse);
+    }
+});
+
+
+
+/*
+TODO:
+    [x] Get query from user
+    [x] Validate query input
+    [x] Pass query to spoonful api
+    [x] Create HTML response page
+    [x] Create links from api response
+    
+*/
+
+// Query grocery API
+app.get('/recipe', async (req, res) => {
+    const userQueryObj = req.query;
+
+    let jsonResponse;
+
+    // Verify query property
+    if (userQueryObj.hasOwnProperty('query')) {
+        
+        jsonResponse = await getSpoonacularAPIResponse(userQueryObj);
+
+    } else {
+        jsonResponse = { 'success': false, 'msg': 'Need to use minimum valid format: /recipe?query=pasta' };
+        res.status(406).type('json').send(jsonResponse);
+    }
+    const recipeArr = jsonResponse.results.map((obj) =>
+        obj.link = `<a href="/recipe/${obj.id}">${obj.title}</a>`);
+  
+    res.render("recipes", { recipeArr: recipeArr });
+   
 });
 
 app.use((req, res) => {
